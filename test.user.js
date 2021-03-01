81c216351c91871befded78594abca4e5daa7514
NoEul <67686234+N0Eul@users.noreply.github.com>
2021-03-01 22:32:54 +0900

    Update test.user.js

--- test.user.js
+++ test.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Test
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @description  This is a test user script
 // @author       NoEul
 // @grant        none
