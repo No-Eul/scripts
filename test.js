6f8eee817f3b4230a1f672dabf93e5b4b37294c1
NoEul <67686234+N0Eul@users.noreply.github.com>
2021-03-01 19:55:39 +0900

    Create test.js

---
+++ test.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         TEst
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http*://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+	'use strict';
+
+	console.log("Hello, world!");
+})();
