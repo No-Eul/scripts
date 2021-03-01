db147955c5a7b751bd7280368140ee1cd5657392
NoEul <67686234+N0Eul@users.noreply.github.com>
2021-03-01 20:27:13 +0900

    Update test.js

--- test.js
+++ test.js
@@ -4,7 +4,7 @@
 // @version      1.0.0
 // @description  This is a test user script
 // @author       NoEul
-// @match        http*://*/*
+// @match        http*://example.com/*
 // @downloadURL  https://github.com/N0Eul/scripts/raw/master/test.js
 // @grant        none
 // ==/UserScript==
