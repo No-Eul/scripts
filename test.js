f85939d2fa2797b392cdc5a51c30a0f8e357db36
NoEul <67686234+N0Eul@users.noreply.github.com>
2021-03-01 20:12:44 +0900

    Update test.js

--- test.js
+++ test.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @downloadURL  https://github.com/N0Eul/scripts/raw/master/test.png
+// @downloadURL  https://github.com/N0Eul/scripts/raw/master/test.js
 // @match        http*://*/*
 // @grant        none
 // ==/UserScript==
